3d8a98a1bfb1193cfe10ebfc62d2c9641752895e
B.D.Heshan Madhuranga <39455525+heshanm27@users.noreply.github.com>
2023-01-30 13:58:45 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  app.enableCors();
+  await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
